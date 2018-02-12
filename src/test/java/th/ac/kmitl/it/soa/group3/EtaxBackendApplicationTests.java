af43b4f00b58dc38fa071239d5e748aee34fdfea
chin8628 <boon.8628@gmail.com>
2018-02-12 17:14:44 +0700

    Update TODO in test file.

--- src/test/java/th/ac/kmitl/it/soa/group3/EtaxBackendApplicationTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/EtaxBackendApplicationTests.java
@@ -9,6 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EtaxBackendApplicationTests {
 
-	// TODO
+	// TODO : Waiting to decide test convention of our team.
 
 }
