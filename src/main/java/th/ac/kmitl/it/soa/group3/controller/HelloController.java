91e7cddb2ca18b594c6cc0b77e55529198fc28ed
chin8628 <boon.8628@gmail.com>
2018-02-12 17:01:53 +0700

    Rename Greeting file to GreetingModel.

--- src/main/java/th/ac/kmitl/it/soa/group3/controller/HelloController.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/controller/HelloController.java
@@ -1,6 +1,6 @@
 package th.ac.kmitl.it.soa.group3.controller;
 
-import th.ac.kmitl.it.soa.group3.model.Greeting;
+import th.ac.kmitl.it.soa.group3.model.GreetingModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +13,8 @@ public class HelloController {
     private AtomicInteger counter = new AtomicInteger();
 
     @GetMapping("/hello")
-    public Greeting hello(@RequestParam(name = "msg", defaultValue = "Hello, World") String msg) {
-        return new Greeting(counter.incrementAndGet(), msg);
+    public GreetingModel hello(@RequestParam(name = "msg", defaultValue = "Hello, World") String msg) {
+        return new GreetingModel(counter.incrementAndGet(), msg);
     }
 
 }
--- src/main/java/th/ac/kmitl/it/soa/group3/model/GreetingModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/GreetingModel.java
@@ -1,11 +1,11 @@
 package th.ac.kmitl.it.soa.group3.model;
 
-public class Greeting {
+public class GreetingModel {
 
     public final int id;
     public final String msg;
 
-    public Greeting(int id, String msg) {
+    public GreetingModel(int id, String msg) {
         this.id = id;
         this.msg = msg;
     }
