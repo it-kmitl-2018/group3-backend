62ef2608614a881e403191f0170c7388375f0965
chin8628 <boon.8628@gmail.com>
2018-02-25 17:06:51 +0700

    [Add] Add IncludedNoteModel and IncludedNoteModelTest

---
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/IncludedNoteModelTest.java
@@ -0,0 +1,24 @@
+package th.ac.kmitl.it.soa.group3.model;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import th.ac.kmitl.it.soa.group3.model.exchangeddocument.IncludedNoteModel;
+
+public class IncludedNoteModelTest {
+
+    private String subject = "E-Tax Invoice";
+    private String content = "This is a content of this subject.";
+
+    @Test
+    public void itShouldGetSubjectAndContentByGetter() {
+
+        IncludedNoteModel.Builder builder = new IncludedNoteModel.Builder();
+        IncludedNoteModel includedNoteModel = builder
+                .withSubject(this.subject)
+                .withContent(this.content)
+                .build();
+
+        assertEquals(subject, includedNoteModel.subject);
+        assertEquals(content, includedNoteModel.content);
+    }
+}
