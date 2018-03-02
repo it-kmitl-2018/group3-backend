c0bae1c54f99250817f476b6a46e600e14904033
wirunpong-j <wirunpong.j@outlook.com>
2018-03-02 16:00:40 +0700

    [Update] Add lombok annotation to SpecifiedTaxRegistrationModel

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModel.java
@@ -1,24 +1,9 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import lombok.Builder;
+
+@Builder(builderClassName = "Builder")
 public class SpecifiedTaxRegistrationModel {
 
     public String id;
-
-    private SpecifiedTaxRegistrationModel(Builder builder) {
-        this.id = builder.id;
-    }
-
-    public static class Builder {
-
-        private String id;
-
-        public Builder withID(String id) {
-            this.id = id;
-            return this;
-        }
-
-        public SpecifiedTaxRegistrationModel build() {
-            return new SpecifiedTaxRegistrationModel(this);
-        }
-    }
 }
--- src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModelTest.java
@@ -11,7 +11,7 @@ public class SpecifiedTaxRegistrationModelTest {
     public void itShouldGetIDByGetter() {
         SpecifiedTaxRegistrationModel.Builder builder = new SpecifiedTaxRegistrationModel.Builder();
         SpecifiedTaxRegistrationModel specTaxRegisModel = builder
-                .withID(this.id)
+                .id(this.id)
                 .build();
 
         assertEquals(this.id, specTaxRegisModel.id);
