5231f5ab5d682787157d5463a374845d07df8809
chin8628 <=>
2018-03-04 00:14:14 +0700

    [Update] Implement Lombok with IncludedNoteModel and PhoneUniversalModel

--- src/main/java/th/ac/kmitl/it/soa/group3/model/exchangeddocument/IncludedNoteModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/exchangeddocument/IncludedNoteModel.java
@@ -1,32 +1,11 @@
 package th.ac.kmitl.it.soa.group3.model.exchangeddocument;
 
+import lombok.Builder;
+
+@Builder(builderClassName = "Builder")
 public class IncludedNoteModel {
 
     public String subject;
     public String content;
 
-    private IncludedNoteModel(Builder builder) {
-        this.subject = builder.subject;
-        this.content = builder.content;
-    }
-
-    public static class Builder {
-
-        private String subject;
-        private String content;
-
-        public Builder withSubject(String subject) {
-            this.subject = subject;
-            return this;
-        }
-
-        public Builder withContent(String content) {
-            this.content = content;
-            return this;
-        }
-
-        public IncludedNoteModel build() {
-            return new IncludedNoteModel(this);
-        }
-    }
 }
--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/PhoneUniversalModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/PhoneUniversalModel.java
@@ -1,25 +1,11 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import lombok.Builder;
+
+@Builder(builderClassName = "Builder")
 public class PhoneUniversalModel {
 
     // Telephone Universal Communication
 
     public String phoneNumber;
-
-    public PhoneUniversalModel(Builder builder) {
-        this.phoneNumber = builder.phoneNumber;
-    }
-
-    public static class Builder {
-        private String phoneNumber;
-
-        public Builder withPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-            return this;
-        }
-
-        public PhoneUniversalModel build() {
-            return new PhoneUniversalModel(this);
-        }
-    }
 }
