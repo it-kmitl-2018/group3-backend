2966178f47652da21b045685a1fb004354129cd8
patcharapon-j <patcharapon.j@outlook.com>
2018-03-03 17:38:41 +0700

    [Add] EmailUriUniversalCommunicationModel

---
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/EmailUriUniversalCommunicationModel.java
@@ -0,0 +1,9 @@
+package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
+
+import lombok.Builder;
+
+@Builder
+public class EmailUriUniversalCommunicationModel {
+
+    private String uriID;
+}
