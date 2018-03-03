e51b1ab8ee8c220ba4d793094ec77142d98920d9
patcharapon-j <patcharapon.j@outlook.com>
2018-03-03 17:53:08 +0700

    [Add] unit test for BuyerOrderReferencedReferencedDocumentModel

---
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/BuyerOrderReferencedReferencedDocumentModelTest.java
@@ -0,0 +1,29 @@
+package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
+
+import org.junit.jupiter.api.Test;
+import th.ac.kmitl.it.soa.group3.model.TypeCode;
+import java.sql.Timestamp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuyerOrderReferencedReferencedDocumentModelTest {
+
+    private String issueAssignedID = "ABC1254";
+    private Timestamp issueDateTime = new Timestamp(System.currentTimeMillis());
+    private TypeCode referenceTypeCode = TypeCode.RECEIPT;
+
+    @Test
+    public void itShouldGetAllInfoByGetter() {
+        BuyerOrderReferencedReferencedDocumentModel.Builder builder =
+                BuyerOrderReferencedReferencedDocumentModel.builder();
+        BuyerOrderReferencedReferencedDocumentModel buyOrderDocumentModel = builder
+                .issueAssignedID(this.issueAssignedID)
+                .issueDateTime(this.issueDateTime)
+                .referenceTypeCode(this.referenceTypeCode)
+                .build();
+
+        assertEquals(this.issueAssignedID, buyOrderDocumentModel.issueAssignedID);
+        assertEquals(this.issueDateTime, buyOrderDocumentModel.issueDateTime);
+        assertEquals(this.referenceTypeCode, buyOrderDocumentModel.referenceTypeCode);
+    }
+}
