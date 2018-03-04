1d2c52acd1ec62835310f1a006c5c85537219f31
chin8628 <=>
2018-03-04 11:52:12 +0700

    [Update] Rename the PhoneUniversalModel to TelephoneUniversalCommunicationModel

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TelephoneUniversalCommunicationModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TelephoneUniversalCommunicationModel.java
@@ -3,9 +3,7 @@ package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 import lombok.Builder;
 
 @Builder(builderClassName = "Builder")
-public class PhoneUniversalModel {
-
-    // Telephone Universal Communication
+public class TelephoneUniversalCommunicationModel {
 
     public String phoneNumber;
 }
--- src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TelephoneUniversalCommunicationModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TelephoneUniversalCommunicationModelTest.java
@@ -3,14 +3,14 @@ package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PhoneUniversalModelTest {
+public class TelephoneUniversalCommunicationModelTest {
 
     private final String phoneNumber = "0818574938";
 
     @Test
     public void itShouldBePhoneNumber() {
-        PhoneUniversalModel.Builder builder = new PhoneUniversalModel.Builder();
-        PhoneUniversalModel phoneNumberModel = builder.phoneNumber(phoneNumber).build();
+        TelephoneUniversalCommunicationModel.Builder builder = new TelephoneUniversalCommunicationModel.Builder();
+        TelephoneUniversalCommunicationModel phoneNumberModel = builder.phoneNumber(phoneNumber).build();
         assertEquals(phoneNumber, phoneNumberModel.phoneNumber);
     }
 
