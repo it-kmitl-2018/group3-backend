022588405272934075c5c1aa43b1b6e04a3d3113
wirunpong-j <wirunpong.j@outlook.com>
2018-03-06 12:19:46 +0700

    [Update] ExchangedDocumentController to post method

--- src/main/java/th/ac/kmitl/it/soa/group3/controller/ExchangedDocumentController.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/controller/ExchangedDocumentController.java
@@ -1,35 +1,24 @@
 package th.ac.kmitl.it.soa.group3.controller;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import th.ac.kmitl.it.soa.group3.model.TypeCode;
-import java.sql.Timestamp;
 import th.ac.kmitl.it.soa.group3.model.exchangeddocument.ExchangedDocumentModel;
-import th.ac.kmitl.it.soa.group3.model.exchangeddocument.IncludedNoteModel;
+import th.ac.kmitl.it.soa.group3.wrapper.ExchangedDocWrapper;
 
 @RestController
 @Api(value = "ExchangedDocument XML", description = "This is ExchangedDocument XML")
 public class ExchangedDocumentController {
 
-    @GetMapping(value = "/exchangeddoc")
-    public ExchangedDocumentModel exchangedDocumentXML() {
-        IncludedNoteModel includedNote = IncludedNoteModel.builder()
-                .subject("หมายเหตุ")
-                .content("ค่าบริการเพิ่มเติม")
-                .build();
-        ExchangedDocumentModel exchangedDocument = ExchangedDocumentModel.builder()
-                .id("RDTIV0575526000058001")
-                .name(TypeCode.TAX_INVOICE.getDescription())
-                .typeCode(TypeCode.TAX_INVOICE.getTypeCode())
-                .issueDateTime(new Timestamp(System.currentTimeMillis()))
-                .purpose("คำนวณราคาค่าบริการผิดพลาดสูงกว่าที่เป็นจริง")
-                .purposeCode("DCNS03")
-                .globalID("ABCDEFGHIJKLMNOPQRST123456789012345")
-                .creationDateTime(new Timestamp(System.currentTimeMillis()))
-                .includedNote(includedNote)
-                .build();
+    @PostMapping(value = "/exchangeddoc")
+    @ApiOperation(value = "Convert ExchangedDocument to XML")
+    public ExchangedDocumentModel exchangedDocumentXML(
+            @ApiParam(value = "ExchangedDocument Object", required = true)
+            @RequestBody ExchangedDocWrapper wrapper) {
 
-        return exchangedDocument;
+        return wrapper.getModelClass();
     }
 }
