03da245ff88b00047968b42d372c9834f9109ae8
wirunpong-j <wirunpong.j@outlook.com>
2018-03-06 12:18:24 +0700

    [Update] ExchangedDocumentControllerTest

--- src/test/java/th/ac/kmitl/it/soa/group3/controller/ExchangedDocumentControllerTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/controller/ExchangedDocumentControllerTest.java
@@ -7,11 +7,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ExchangedDocumentControllerTest {
+
+    private String requestBody = "{}";
 
     static private MockMvc mockMvc;
 
@@ -25,8 +27,10 @@ public class ExchangedDocumentControllerTest {
 
     @Test
     void shouldRequestAndGetXmlResponse() throws Exception {
-        mockMvc.perform(get("/exchangeddoc")
-                .contentType(MediaType.APPLICATION_XML))
+        mockMvc.perform(post("/exchangeddoc")
+                .accept(MediaType.APPLICATION_XML)
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_XML));
     }
