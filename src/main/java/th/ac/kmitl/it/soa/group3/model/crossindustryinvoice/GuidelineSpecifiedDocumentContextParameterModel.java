656e22f275dbd54e524d0c1a8c181230c1e69c75
patcharapon-j <patcharapon.j@outlook.com>
2018-03-06 11:41:51 +0700

    [Add] GuidelineSpecifiedDocumentContextParameterModel

---
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/crossindustryinvoice/GuidelineSpecifiedDocumentContextParameterModel.java
@@ -0,0 +1,13 @@
+package th.ac.kmitl.it.soa.group3.model.crossindustryinvoice;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Builder;
+
+@JacksonXmlRootElement(localName = "ram:GuidelineSpecifiedDocumentContextParameter")
+@Builder(builderClassName = "builder")
+public class GuidelineSpecifiedDocumentContextParameterModel {
+
+    @JacksonXmlProperty(localName = "ram:ID")
+    public String id;
+}
