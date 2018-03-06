d69008b538c3d40ce9d056d89dcda62b5f669399
patcharapon-j <patcharapon.j@outlook.com>
2018-03-06 11:45:39 +0700

    [Add] ExchangedDocumentContextModel

---
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/crossindustryinvoice/ExchangedDocumentContextModel.java
@@ -0,0 +1,13 @@
+package th.ac.kmitl.it.soa.group3.model.crossindustryinvoice;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Builder;
+
+@JacksonXmlRootElement(localName = "rsm:ExchangedDocumentContext")
+@Builder(builderClassName = "builder")
+public class ExchangedDocumentContextModel {
+
+    @JacksonXmlProperty
+    public GuidelineSpecifiedDocumentContextParameterModel  guidelineSpecifiedDocumentContextParameterModel;
+}
