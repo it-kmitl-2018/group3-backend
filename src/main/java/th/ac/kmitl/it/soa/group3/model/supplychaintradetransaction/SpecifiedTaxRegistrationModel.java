65569c4a022575071e02626fa82293a15b6ae9cb
Patcharapon Joksamut <vampirefeem@gmail.com>
2018-04-25 14:45:54 +0700

    [Update] Annotate json, xml to SpecifiedTaxRegistrationModel and unit test

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModel.java
@@ -1,9 +1,13 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.Builder;
 
 @Builder(builderClassName = "Builder")
 public class SpecifiedTaxRegistrationModel {
 
+    @JacksonXmlProperty(localName = "ram:ID")
+    @JsonProperty("id")
     public String id;
 }
--- src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/SpecifiedTaxRegistrationModelTest.java
@@ -1,19 +1,42 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.gson.Gson;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SpecifiedTaxRegistrationModelTest {
 
     private String id = "1234567890123";
+    private SpecifiedTaxRegistrationModel specTaxRegister;
+
+    @Before
+    public void beforeEachTest() {
+        this.specTaxRegister = new SpecifiedTaxRegistrationModel.Builder()
+                .id(this.id)
+                .build();
+    }
 
     @Test
     public void itShouldGetIDByGetter() {
-        SpecifiedTaxRegistrationModel.Builder builder = new SpecifiedTaxRegistrationModel.Builder();
-        SpecifiedTaxRegistrationModel specTaxRegisModel = builder
-                .id(this.id)
-                .build();
+        assertEquals(this.id, this.specTaxRegister.id);
+    }
 
-        assertEquals(this.id, specTaxRegisModel.id);
+    @Test
+    public void itShouldGetXMLString() throws JsonProcessingException {
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(this.specTaxRegister);
+
+        assertNotNull(xml);
+    }
+
+    @Test
+    public void itShouldGetJSONString() {
+        Gson gson = new Gson();
+        String json = gson.toJson(this.specTaxRegister);
+
+        assertNotNull(json);
     }
 }
