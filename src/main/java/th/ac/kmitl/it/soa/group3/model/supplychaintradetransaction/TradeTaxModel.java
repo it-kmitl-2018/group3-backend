528a6f3999dc71057cd7d270ce22fe550e3b6372
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2018-05-13 16:13:13 +0700

    [Update] Annotate XML to TradeTaxModel with unit test

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeTaxModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeTaxModel.java
@@ -1,12 +1,22 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
 import lombok.Builder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+@JacksonXmlRootElement(localName = "rsm:ApplicableHeaderTradeDelivery")
 @Builder(builderClassName = "Builder")
 public class TradeTaxModel {
 
+    @JacksonXmlProperty(localName = "ram:TypeCode")
     public String typeCode;
+
+    @JacksonXmlProperty(localName = "ram:CalculatedRate")
     public double calculatedRate;
+
+    @JacksonXmlProperty(localName = "ram:BasisAmount")
     public double basisAmount;
+
+    @JacksonXmlProperty(localName = "ram:CalculatedAmount")
     public double calculatedAmount;
 }
--- src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeTaxModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeTaxModelTest.java
@@ -1,5 +1,8 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,20 +12,37 @@ public class TradeTaxModelTest {
     private double calculatedRate = 0.07;
     private double basisAmount = 100.00;
     private double calculatedAmount = 7.00;
+    private TradeTaxModel tradeTaxModel;
+    private String expectedXML = "<rsm:ApplicableHeaderTradeDelivery>" +
+            "<ram:TypeCode>VAT</ram:TypeCode>" +
+            "<ram:CalculatedRate>0.07</ram:CalculatedRate>" +
+            "<ram:BasisAmount>100.0</ram:BasisAmount>" +
+            "<ram:CalculatedAmount>7.0</ram:CalculatedAmount>" +
+            "</rsm:ApplicableHeaderTradeDelivery>";
 
-    @Test
-    public void expectTradingTaxInfo() {
-        TradeTaxModel.Builder builder =  new TradeTaxModel.Builder();
-        TradeTaxModel tradeTaxModel = builder
+    @Before
+    public void beforeEachTest() {
+        this.tradeTaxModel = new TradeTaxModel.Builder()
                 .typeCode(this.typeCode)
                 .calculatedRate(this.calculatedRate)
                 .basisAmount(this.basisAmount)
                 .calculatedAmount(this.calculatedAmount)
                 .build();
+    }
 
-        assertEquals(this.typeCode, tradeTaxModel.typeCode);
-        assertEquals(this.calculatedRate, tradeTaxModel.calculatedRate);
-        assertEquals(this.basisAmount, tradeTaxModel.basisAmount);
-        assertEquals(this.calculatedAmount, tradeTaxModel.calculatedAmount);
+    @Test
+    public void expectTradingTaxInfo() {
+        assertEquals(this.typeCode, this.tradeTaxModel.typeCode);
+        assertEquals(this.calculatedRate, this.tradeTaxModel.calculatedRate);
+        assertEquals(this.basisAmount, this.tradeTaxModel.basisAmount);
+        assertEquals(this.calculatedAmount, this.tradeTaxModel.calculatedAmount);
+    }
+
+    @Test
+    public void itShouldGetXMLString() throws JsonProcessingException {
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(this.tradeTaxModel);
+
+        assertEquals(this.expectedXML, xml);
     }
 }
