d563cee5cdcfefc7dbb7c4f920c8df497685c1fd
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2018-05-13 15:52:45 +0700

    [Update] Annotate XML to ActualDeliverySupplyChainEventModel with unit test

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/ActualDeliverySupplyChainEventModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/ActualDeliverySupplyChainEventModel.java
@@ -1,10 +1,15 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
 import java.sql.Timestamp;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Builder;
 
+@JacksonXmlRootElement(localName = "rsm:ActualDeliverySupplyChainEvent")
 @Builder(builderClassName = "Builder")
 public class ActualDeliverySupplyChainEventModel {
 
+    @JacksonXmlProperty(localName = "ram:OccurrenceDateTime")
     public Timestamp occurrenceDateTime;
 }
--- src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/ActualDeliverySupplyChainEventModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/ActualDeliverySupplyChainEventModelTest.java
@@ -1,20 +1,37 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.junit.Before;
 import org.junit.Test;
 import java.sql.Timestamp;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ActualDeliverySupplyChainEventModelTest {
 
-    private Timestamp occurrenceDateTime = new Timestamp(System.currentTimeMillis());
+    private Timestamp occurrenceDateTime = new Timestamp(1526201421814l);
+    private ActualDeliverySupplyChainEventModel deliverySupplyChainModel;
+    private String expectedXML = "<rsm:ActualDeliverySupplyChainEvent>" +
+            "<ram:OccurrenceDateTime>1526201421814</ram:OccurrenceDateTime>" +
+            "</rsm:ActualDeliverySupplyChainEvent>";
+
+    @Before
+    public void beforeEachTest() {
+        this.deliverySupplyChainModel = new ActualDeliverySupplyChainEventModel.Builder()
+                .occurrenceDateTime(this.occurrenceDateTime)
+                .build();
+    }
 
     @Test
     public void itShouldGetDateTimeByGetter() {
-        ActualDeliverySupplyChainEventModel.Builder builder = new ActualDeliverySupplyChainEventModel.Builder();
-        ActualDeliverySupplyChainEventModel deliverySupplyChainModel = builder
-                .occurrenceDateTime(this.occurrenceDateTime)
-                .build();
+        assertEquals(this.occurrenceDateTime, this.deliverySupplyChainModel.occurrenceDateTime);
+    }
 
-        assertEquals(this.occurrenceDateTime, deliverySupplyChainModel.occurrenceDateTime);
+    @Test
+    public void itShouldGetXMLString() throws JsonProcessingException {
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(this.deliverySupplyChainModel);
+
+        assertEquals(this.expectedXML, xml);
     }
 }
