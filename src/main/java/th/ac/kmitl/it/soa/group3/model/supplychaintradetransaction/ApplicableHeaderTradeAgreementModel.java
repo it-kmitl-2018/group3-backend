322bd8cf6ed86c803ac7f35ca67484c6c1b31c0d
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2018-05-13 15:34:16 +0700

    [Update] Annotate XML to ApplicableHeaderTradeAgreementModel with unit test

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/ApplicableHeaderTradeAgreementModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/ApplicableHeaderTradeAgreementModel.java
@@ -1,13 +1,25 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.Builder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 @Builder(builderClassName = "Builder")
+@JacksonXmlRootElement(localName = "rsm:ApplicableHeaderTradeAgreement")
 public class ApplicableHeaderTradeAgreementModel {
 
+    @JacksonXmlProperty(localName = "ram:SellerTradeParty")
     public TradePartyModel sellerTradeParty;
+
+    @JacksonXmlProperty(localName = "ram:BuyerTradeParty")
     public TradePartyModel buyerTradeParty;
+
+    @JacksonXmlProperty(localName = "ram:ApplicableTradeDeliveryTerms")
     public TradeDeliveryTermsModel tradeDeliveryTerms;
+
+    @JacksonXmlProperty(localName = "ram:BuyerOrderReferencedReferencedDocument")
     public BuyerOrderReferencedReferencedDocumentModel buyerOrderReferencedReferencedDocument;
+
+    @JacksonXmlProperty(localName = "ram:AdditionalReferencedDocument")
     public BuyerOrderReferencedReferencedDocumentModel additionalReferencedDocument;
 }
