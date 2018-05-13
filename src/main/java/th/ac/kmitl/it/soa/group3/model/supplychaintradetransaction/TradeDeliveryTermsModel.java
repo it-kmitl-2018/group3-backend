d5e098bc356267cf181eee231e5aa31dadc3ed2d
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2018-05-13 15:04:14 +0700

    [Update] Annotate XML to TradeDeliveryTermsModel with unit test

--- src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeDeliveryTermsModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeDeliveryTermsModel.java
@@ -1,9 +1,13 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Builder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+@JacksonXmlRootElement(localName = "rsm:ApplicableTradeDeliveryTerms")
 @Builder(builderClassName = "Builder")
 public class TradeDeliveryTermsModel {
 
+    @JacksonXmlProperty(localName = "ram:DeliveryTypeCode")
     public String deliveryTypeCode;
 }
--- src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeDeliveryTermsModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group3/model/supplychaintradetransaction/TradeDeliveryTermsModelTest.java
@@ -1,11 +1,17 @@
 package th.ac.kmitl.it.soa.group3.model.supplychaintradetransaction;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TradeDeliveryTermsModelTest {
 
     private String deliveryTypeCode = "DDP";
+    private String expectedXML = "<rsm:ApplicableTradeDeliveryTerms>" +
+            "<ram:DeliveryTypeCode>DDP</ram:DeliveryTypeCode>" +
+            "</rsm:ApplicableTradeDeliveryTerms>";
 
     @Test
     public void expectDeliveryTypeCode() {
@@ -15,5 +21,18 @@ public class TradeDeliveryTermsModelTest {
                 .build();
 
         assertEquals(this.deliveryTypeCode, tradeDeliveryTermsModel.deliveryTypeCode);
+    }
+
+    @Test
+    public void itShouldGetXMLString() throws JsonProcessingException {
+        TradeDeliveryTermsModel.Builder builder = new TradeDeliveryTermsModel.Builder();
+        TradeDeliveryTermsModel tradeDeliveryTermsModel = builder
+                .deliveryTypeCode(this.deliveryTypeCode)
+                .build();
+
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(tradeDeliveryTermsModel);
+
+        assertEquals(this.expectedXML, xml);
     }
 }
